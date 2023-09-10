a8ff7d73e57731976782e0f7067ff1afaabd1b69
Akash Khandelwal <skykhandelwal1999@gmail.com>
2023-09-11 00:43:00 +0530

    Image Sent through Email Feature Added

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,6 +2,7 @@ class UserMailer < ApplicationMailer
 
 	def welcome_mail(user)
      @user = user
+     attachments.inline['online-reselling.png'] = File.read('/home/rails-17/Desktop/Akash/Ecommerce/app/assets/images/online-reselling.png')
      mail(to: email_address_with_name(@user.email, @user.first_name),subject: "Welcome To Let's Shop")
   end
 
